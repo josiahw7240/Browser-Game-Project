2d5bb86dc95eeadc725bbbc763b54ad8c6cf59c6
josiahw7240 <71360953+josiahw7240@users.noreply.github.com>
2022-03-09 14:57:56 -0500

    js update before fifa break

---
+++ script.js
@@ -0,0 +1,19 @@
+let blackJackGame = {
+    'you' :
+    {
+        'scoreSpan' : '#your-blackjack-result',
+        'div' : '#your-box',
+        'boxsize' : '.flex-blackjack-row-2 div'
+        'score' : 0
+    },
+
+    'dealer' :
+    {
+        'scoreSpan' : '#dealer-blackjack-result',
+        'div' : '#dealer-box',
+        'boxsize' : '.flex-black-row-2 div',
+        'score' : 0
+    },
+
+    cards: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+}
